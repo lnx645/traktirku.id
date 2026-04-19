17b009c6b7bc278471f384e62a3248261008b482
lnx645 <dadanhidyt@gmail.com>
2026-04-19 19:31:18 +0700

    first commit

--- resources/ts/components/BaseNavbar.tsx
+++ resources/ts/components/BaseNavbar.tsx
@@ -1,25 +1,27 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { ReactNode } from 'react';
-
-const headerStyles = css`
+import styled from '@emotion/styled';
+import mq from '@/lib/breakpoints';
+const Wrapper = styled.div`
     label: header;
     background: white;
     box-shadow: 0px 0px 8px #dedede;
     height: var(--h-navbar);
 `;
-const headerWrapper = css`
-    height: var(--h-navbar);
-    display: flex;
-    padding-inline: 3rem;
-    align-items: center;
-`;
-export const BaseNavbar = ({children}:{children:ReactNode}) => {
+const NavbarWrapperMain = styled.div(
+    mq({
+        display: 'flex',
+        alignItems: 'center',
+        height: 'var(--h-navbar)',
+        paddingInline: [10, 42],
+    }),
+);
+
+export const BaseNavbar = ({ children }: { children: ReactNode }) => {
     return (
-        <div css={headerStyles}>
-            <div css={headerWrapper}>
-                {children}
-            </div>
-        </div>
+        <Wrapper>
+            <NavbarWrapperMain>{children}</NavbarWrapperMain>
+        </Wrapper>
     );
 };
